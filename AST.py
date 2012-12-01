178fc738d5a25ea16e8431f3f6b887a1a6185804
Irina Veliche <iv511@ic.ac.uk>
2012-12-01 13:35:06 +0000

    AST.py second version

--- AST.py
+++ AST.py
@@ -10,7 +10,7 @@ BODY             = "body"
 FORMAL_PARAMS    = "formal_params"
 
 class ASTNode(object):
-	def __init__(self, nodeType, line, lexpos, children):
+	def __init__(self, nodeType, children, line, lexpos):
 		self.nodeType = nodeType
 		self.line     = line
 		self.lexpos   = lexpos
@@ -19,20 +19,20 @@ class ASTNode(object):
 	def getNodeType(self):
 		return self.nodeType
 
-class CompilationUnit(self):
-	def __init__(self, nodeType, line, lexpos, children):
-		super(CompilationUnit, self).__init__(COMPILATION_UNIT, line, lexpos, children)
+class CompilationUnit(ASTNode):
+	def __init__(self, nodeType, children, line, lexpos):
+		super(CompilationUnit, self).__init__(COMPILATION_UNIT, line, lexpos)
 	
 	def getDecls(self):
 		return self.children[0]
 
-class Decls(self):
-	def __init__(self, line, lexpos, children):
-		super(DeclNode, self).__init__(DECLARATION, line, lexpos, children)
+class Decls(ASTNode):
+	def __init__(self, line, lexpos):
+		super(DeclNode, self).__init__(DECLARATION, line, lexpos)
 
-def Body(self):
-    def __init__(self, nodeType, lien, lexpos, children):
-        super(Body, self).__init__(BODY, line, lexpos, children)
+class Body(ASTNode):
+    def __init__(self, nodeType, lien, lexpos):
+        super(Body, self).__init__(BODY, line, lexpos)
 
     def getDecls(self):
         return self.children[0]
@@ -40,21 +40,21 @@ def Body(self):
     def getStatement(self):
         return self.children[1]
 
-def FormalParams(self):
-    def __init__(self, line, lexpos, children):
-        super(FormalParams, self).__init__(FORMAL_PARAMS, line, lexpos, children)
+class FormalParams(ASTNode):
+    def __init__(self, line, lexpos):
+        super(FormalParams, self).__init__(FORMAL_PARAMS, line, lexpos)
 
 
-class VarDecl(StatementList):
-	def __init__(self, line, lexpos, children):
-		super(VarDecl, self).__init__(DECLARATION, line, lexpos, children)
+class VarDecl(ASTNode):
+	def __init__(self, line, lexpos):
+		super(VarDecl, self).__init__(DECLARATION, line, lexpos)
 
 	def getType(self):
 		return self.children[1]
 
-class ArrayDecl(DeclNode):
-	def __init__(self, line, lexpos, children):
-		super(ArrayDecl, self).__init__(DECLARATION, line, lexpos, children)
+class ArrDecl(ASTNode):
+	def __init__(self, line, lexpos):
+		super(ArrDecl, self).__init__(DECLARATION, line, lexpos)
 
 	def getSizeExpr(self):
 		return self.children[0]
@@ -62,35 +62,80 @@ class ArrayDecl(DeclNode):
 	def getBaseType(self):
 		return self.children[1]
 
-def CompoundStatement(ASTNOde):
-	def __init__(self, nodeType, line, lexpos, chilren):
-		super(CompoundStatement, self).__init__(nodeType, line, lexpos, children)
+def FunDecl(Decls):
+	def __init__(self, nodeType, children, line, lexpos):
+		super(FunDecl, self).__init__(nodeType, children, line, lexpos)
 
-	#def getStatement(self):	
+	def getFormalParams(self):
+		return self.children[0]
+
+	def getType(self):
+		return self.children[1]
+		
+	def getBody(self):
+		return self.children[2]
+
+class ProcDecl(Decls):
+	def __init__(self, nodeType, children, line, lexpos):
+		super(ProcDecl, self).__init__(nodeType, children, line, lexpos)
+
+	def getFormalParams(self):
+		return self.children[0]
+
+	def getBody(self):
+		return self.children[1]
+
+class FunParams(self):
+	def __init__(self, nodeType, children, line, lexpos):
+		super(FunParams, self).__init__(nodeType, children, line, lexpos)
+
+	def getExpr(self):
+		return self.children[0]
+
+class CallParams(self):
+	def __init__(self, nodeType, children, line, lexpos):
+		super(CallParams, self).__init__(nodeType, children, line, lexpos)
+
+	def getLeftExpr(self):
+        return self.children[0]
+
+    def getRightExpr(self):
+        return self.children[1]
+
+
+class CompoundStatement(ASTNOde):
+	def __init__(self, nodeType, children, line, lexpos, chilren):
+		super(CompoundStatement, self).__init__(nodeType, children, line, lexpos)
+
+	def getStatement(self):	
+		return self.children[0]
+
+	def getStatementList(self):
+		return self.childre[1]		
 
 #Statements
 
-def PrintStatement(CompoundStatement):
-	def __init__(self, nodeType, line, lexpos, children):
-		super(PrintStatement, self).__init__(nodeType, line, lexpos, children)
+class PrintStatement(CompoundStatement):
+	def __init__(self, nodeType, children, line, lexpos):
+		super(PrintStatement, self).__init__(nodeType, children, line, lexpos)
 
 	def getExpr(self):
 		return self.children[0]
 
-def ReadStatement(CompoundStatement):
-	def __init__(self, nodeType, line, lexpos, children):
-		super(ReadStatement, self).__init__(nodeType, line, lexpos, children)
+class ReadStatement(CompoundStatement):
+	def __init__(self, nodeType, children, line, lexpos):
+		super(ReadStatement, self).__init__(nodeType, children, line, lexpos)
 
 	def getExpr(self):
 		return self.children[0]
 
-def NullStatement(CompoundStatement):
-	def __init__(self, nodeType, line, lexpos, children):
-		super(NullStatement, self).__init__(nodeType, line, lexpos, children)
+class NullStatement(CompoundStatement):
+	def __init__(self, nodeType, children, line, lexpos):
+		super(NullStatement, self).__init__(nodeType, children, line, lexpos)
 
-def AssignStatement(CompoundStatement):
-	def __init__(self, nodeType, line, lexpos, children):
-		super(AssignStatement, self).__init__(nodeType, line, lexpos, children)
+class AssignStatement(CompoundStatement):
+	def __init__(self, nodeType, children, line, lexpos):
+		super(AssignStatement, self).__init__(nodeType, children, line, lexpos)
 
 	def getLeftExpr(self):
 		return self.children[0]
@@ -98,34 +143,78 @@ def AssignStatement(CompoundStatement):
 	def getRightExpr(self):
 		return self.children[1]
 
-def ActualParams(self):
-	def __init__(self, nodeType, line, lexpos, children):
-		super(ActualParams, self).__init__(nodeType, line, lexpos, children)
+class IncrementStatement(CompoundStatement):
+	def __init__(self, nodeType, children, line, lexpos):
+		super(IncrementStatement, self).__init__(nodeType, children, line, lexpos)
 
-	def getLeftExpr(self):
+	def getExpr(self):
+		return self.childre[0]
+
+class DecrementStatement(self):
+	def __init__(self, nodeType, children, line, lexpos):
+		super(DecrementStatement, self).__init__(nodeType, children, line, lexpos)
+
+	def getExpr(self):
 		return self.children[0]
 
-	def getRightExpr(self):
+class ReturnStatement(self):
+    def __init__(self, nodeType, children, line, lexpos):
+        super(ReturnStatement, self).__init__(nodeType, children, line, lexpos)
+
+    def getExpr(self):
+        return self.children[0]
+
+class CallStatement(self):
+    def __init__(self, nodeType, children, line, lexpos):
+        super(CallStatement, self).__init__(nodeType, children, line, lexpos)
+
+    def getFunParams(self):
+		return self.children[0].children
+
+	def NumberOfFunParams(self):
+		return len(self.childre[0].children)
+
+class LoopStatement(self):
+	def __init__(self, nodeType, children, line, lexpos):
+		super(LoopStatement, self).__init__(nodeType, children, line, lexpos)
+
+	def getExpr(self):
+		return self.children[0]
+
+	def getCompoundStatement(self):
 		return self.children[1]
 
-def Operator(self):
-	def __init__(self, nodeType, line, lexpos, operator, children):
-		super(Operator, self).__init__(nodeType, line, lexpos, children)
+class IfStatement(self):
+	def __init__(self, nodeType, children, line, lexpos):
+		super(IfStatement, self).__init__(nodeType, children, line, lexpos)
+
+	def getExpr(self):
+		return self.children[0]
+
+	def getStatement1(self):
+		return self.children[1]
+
+	def getStatement2(self):
+		return self.children[2] 
+
+class Operator(ASTNode):
+	def __init__(self, nodeType, children, line, lexpos, operator, children):
+		super(Operator, self).__init__(nodeType, children, line, lexpos)
 		self.operator = operator
 
 	def getOperator(self):
 		return self.operator
 
-def UnaryNode(Operator):
-	def __init__(self, line, lexpos, operator, children):
-		super(UnaryOperator, self).__init__(UNARY_OP, line, lexpos, operaotr, children)
+class UnaryExpr(Operator):
+	def __init__(self, children, line, lexpos, operator):
+		super(UnaryExpr, self).__init__(UNARY_OP, children, line, lexpos, operator)
 
 	def getExpr(self):
 		return self.children[0]
 
-def BinaryNode(Operator):
-	def __init__(self, line, lexpos, operator, children):
-		super(BinaryNode, self).__init__(BINARY_OP, line, lexpos, children)
+class BinaryExpr(Operator):
+	def __init__(self, children, line, lexpos, operator):
+		super(BinaryExpr, self).__init__(BINARY_OP, line, lexpos)
 
 	def getLeftExpr(self):
 		return self.children[0]
@@ -133,9 +222,44 @@ def BinaryNode(Operator):
 	def getRightExpr(self):
 		return self.children[1]
 
-def Factor(self):
-	def __init__(self, nodeType, line, lexpos, children):
-		super(Factor, self).__init__(FACTOR, line, lexpos, children)
+class ArrExpr(self):
+	def __init__(self, nodeType, children, line, lexpos):
+		super(ArrExpr, self).__init__(nodeType, children, line, lexpos)
+
+	def getExpr(self):
+		return self.children[0]
+
+class CallExpr(self):
+	def __init__(self, nodeType, children, line, lexpos):
+		super(CallExpr, self).__init__(nodeType, children, line, lexpos)
+
+	def getCallParams(self):
+		return self.children[0]
+
+class IntExpr(self):
+	def __init__(self, nodeType, children, line, lexpos):
+        super(IntExpr, self).__init__(nodeType, children, line, lexpos)
+	
+	def getExpr(self):
+		return self.children[0]
+
+class CharExpr(self):
+	def __init__(self, nodeType, children, line, lexpos):
+        super(CharExpr, self).__init__(nodeType, children, line, lexpos)
+
+	def getExpr(self):
+		return self.children[0]
+
+class StringExpr(self):
+	def __init__(self, nodeType, children, line, lexpos):
+        super(StringExpr, self).__init__(nodeType, children, line, lexpos)
+	
+	def getExpr(self):
+		return self.children[0]
+
+class Factor(ASTNode):
+	def __init__(self, nodeType, children, line, lexpos):
+		super(Factor, self).__init__(FACTOR, line, lexpos)
 		self.factorType = nodeType
 
 	def getFactor(self):
@@ -144,26 +268,26 @@ def Factor(self):
 	def getVal(self):
 		return self.children[0]
 
-def Number(Factor):
-	def __init__(self, line, lexpos, children):
-		super(Number, self).__init__(NUMBER, line, lexpos, children)
+class Number(Factor):
+	def __init__(self, line, lexpos):
+		super(Number, self).__init__(NUMBER, line, lexpos)
 
-def Id(Factor):
-	def __init__(self, line, lexpos, children):
-		super(Id, self).__init__(ID, line, lexpos, children)
+class Id(Factor):
+	def __init__(self, line, lexpos):
+		super(Id, self).__init__(ID, line, lexpos)
 
-def Letter(Factor):
-	def __init__(self, line, lexpos, children):
-		super(Letter, self).__init__(LETTER, line, lexpos, children)
+class Letter(Factor):
+	def __init__(self, line, lexpos):
+		super(Letter, self).__init__(LETTER, line, lexpos)
 
-def IntType():
-	def __init__(self, nodeType, line, lexpos, children):
-		super(IntType, self).__init__(nodeType, line, lexpos, children)
+class IntType():
+	def __init__(self, nodeType, children, line, lexpos):
+		super(IntType, self).__init__(nodeType, children, line, lexpos)
 
-def CharType():
-	def __init__(self, nodeType, line, lexpos, children):
-		super(CharType, self).__init__(nodeType, line, lexpos, children)
+class CharType():
+	def __init__(self, nodeType, children, line, lexpos):
+		super(CharType, self).__init__(nodeType, children, line, lexpos)
 
-def StringType():
-	def __init__(self, nodeType, line, lexpos, children):
-		super(StringType, self).__init__(nodeType, line, lexpos, children)
+class StringType():
+	def __init__(self, nodeType, children, line, lexpos):
+		super(StringType, self).__init__(nodeType, children, line, lexpos)
